3b973a50d53db9a3aa16052598255fa63ac75dae
ManojGitHub6 <manojeligeti666@gmail.com>
2022-01-01 13:17:25 +0530

    Added Branch_01

--- appSelect/src/appSelect/Class_A.java
+++ appSelect/src/appSelect/Class_A.java
@@ -5,6 +5,8 @@ public class Class_A {
 	public static void main(String[] args) {
 
 		System.out.println("Class_A");
+		System.out.println("Added branch_01");
+		System.out.println("Added Branch 1");
 	}
 
 }
